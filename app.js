e73fd59e65311293a27a338969dc13148f64591e
ifiszer <ivan.fiszer@gmail.com>
2017-01-19 20:34:22 -0300

    insert song
    Agregado bodyParser
    Post agrega una cancion, si ya existe el album la anexa al mismo.

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 var neo4j = require('./neo4j/Neo4JManager').driver;
 
-app.get('/', function (req, res) {
+var app = express();
+app.use(bodyParser.json());
+
+
+app.post('/music/song', function (req, res) {
 	var session = neo4j.session();
 	session
-	  .run( "CREATE (a:Person {name: {name}, title: {title}})", {name: "Arthur", title: "King"});
-	  .then( function() {
-	    return session.run( "MATCH (a:Person) WHERE a.name = {name} RETURN a.name AS name, a.title AS title",
-	        {name: "Arthur"});
+	  .run( `MERGE (a:Album {name: {album}, artist:{artist}}) 
+	  		 MERGE (:Song {name: {name}, track: {track}, genre: {genre}})<-[:Contains]-(a)`, req.body)
+	   .then( function() {
+	    res.status(200).send( "Created OK" );	
+	    session.close();    	   
 	  })
-	  .then( function( result ) {
-	  	console.log(result);
-	    res.send( result.records[0].get("title") + " " + result.records[0].get("name") );
-	    session.close();
-	  });
-});
+	  .catch(function(err){
+	  	res.status(500).send( err );
+	  	session.close();
+	  })
+});	
 
 app.listen(80, function () {
   console.log('Example app listening on port 80!')
