cc033567d69c021eec9baa32092c0f5e9f4f2077
ifiszer <ivan.fiszer@gmail.com>
2017-01-20 14:42:42 -0300

    Separado Artista de Album
    Separado el Artista del Album y agregados get encadenados desde "todos
    los artistas" hasta "las canciones de un album"
    
    Empieza a ser importante generalizar la ejecución de queries

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ app.use(bodyParser.json());
 app.post('/music/song', function (req, res) {
 	var session = neo4j.session();
 	session
-	  .run( `MERGE (a:Album {name: {album}, artist:{artist}}) 
-	  		 CREATE (:Song {name: {name}, track: {track}, genre: {genre}})<-[:Contains]-(a)`, req.body)
+	  .run( `MERGE (artist:Artist{name:{artist}})
+	  		 MERGE (album:Album {name: {album}})<-[:Composed_by]-(artist)
+	  		 CREATE (:Song {name: {name}, track: {track}, genre: {genre}})<-[:Contains]-(album)`, req.body)
 	   .then( function() {
 	    res.status(200).send( "Created OK" );	
 	    session.close();    	   
@@ -23,6 +24,52 @@ app.post('/music/song', function (req, res) {
 	  	session.close();
 	  })
 });	
+
+app.get('/music/artist', function (req, res) {
+	var session = neo4j.session();
+	return session.run( `MATCH (a:Artist) 
+						 RETURN a.name AS artistName`)
+	   .then( function(result) {
+	    res.status(200).json( result.records.map((record)=>{return record.get("artistName")}) );
+	    session.close();    	   
+	  })
+	  .catch(function(err){
+	  	res.status(500).send( err );
+	  	session.close();
+	  })
+});
+
+app.get('/music/artist/:artistName/album', function (req, res) {
+	var session = neo4j.session();
+	return session.run( `MATCH (artist:Artist{name:{artistName}})
+						 MATCH (album:Album)<-[:Composed_by]-(artist) 
+						 RETURN album.name AS albumName`, {artistName:req.params.artistName})
+	   .then( function(result) {
+	    res.status(200).json( result.records.map((record)=>{return record.get("albumName")}) );
+	    session.close();    	   
+	  })
+	  .catch(function(err){
+	  	res.status(500).send( err );
+	  	session.close();
+	  })
+});	
+
+app.get('/music/artist/:artistName/album/:albumName/song', function (req, res) {
+	var session = neo4j.session();
+	return session.run( `MATCH (artist:Artist{name:{artistName}})
+						 MATCH (album:Album{name:{albumName}})<-[:Composed_by]-(artist) 
+						 MATCH (song:Song)<-[:Contains]-(album)
+						 RETURN song AS song ORDER BY song.track ASC`, 
+						 {artistName:req.params.artistName, albumName:req.params.albumName})
+	   .then( function(result) {
+	    res.status(200).json( result.records.map((record)=>{return record.get("song").properties}) );
+	    session.close();    	   
+	  })
+	  .catch(function(err){
+	  	res.status(500).send( err );
+	  	session.close();
+	  })
+});		
 
 app.get('/music/song/:id', function (req, res) {
 	var session = neo4j.session();
